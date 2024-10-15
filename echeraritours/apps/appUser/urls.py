12a7fadc26a8f7d3729be50395654b640107b83e
LeonardoOrtegaEscobar <143544582+LeonardoOrtegaEscobar@users.noreply.github.com>
2024-10-15 01:46:38 -0600

    Añadi las vistas para el registro de agencia.
    Cree dos archivos css agencia_register y validar_agencias.
    Ademas agruege las vistas html a esas dos funcionalidades. Faltan correciones y mejoras de diseño.

--- echeraritours/apps/appUser/urls.py
+++ echeraritours/apps/appUser/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('agencia_registro/', views.agencia_registro),
+    path('validar_agencia/', views.validar_agencia),
 ]
--- echeraritours/apps/appUser/views.py
+++ echeraritours/apps/appUser/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def agencia_registro(request):
+    return render(request, 'agencia_registro.html')
+
+def validar_agencia(request):
+    if request.method == 'POST':
+        certificado = request.FILES.get('certificado')
+        # Aquí puedes guardar el archivo o realizar la validación.
+        return HttpResponse('Certificado recibido.')
+    return render(request, 'validar_agencia.html')
