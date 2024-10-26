a8f3f0bd657ecd88eb3c5f38bde7e5f3962809f5
226129 <al226129@alumnos.uacj.mx>
2024-10-25 18:13:01 -0600

    Agregue el filtrado de los tours en la seccion de tours. Agregue la funcionalidad para que aparezcan todos los tours desde la base de datos. Agregue la funcionalidad con class based views para ver la informacion de cada tour en especifico

--- echeraritours/apps/appTour/views.py
+++ echeraritours/apps/appTour/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView
 
 from .models import Tour
+from django.db.models import Q
 
 # Create your views here.
+
+
 def tours(request):
-    return render(request, 'tour_templates/tours.html')
+    tours = Tour.objects.all()
+
+    # Filtrar por precio máximo
+    precio_max = request.GET.get('precio')
+    if precio_max:
+        tours = tours.filter(price_per_person__lte=precio_max)
+
+    # Filtrar por lugar (en destino o lugar de origen)
+    lugar = request.GET.get('lugar')
+    if lugar:
+        tours = tours.filter(
+            Q(destination_place__icontains=lugar) |
+            Q(place_of_origin__icontains=lugar)
+        )
+
+    return render(request, 'tour_templates/tours.html', {'tours': tours})
+
+
+class TourDetailView(DetailView):
+    model = Tour
+    template_name = 'tour_templates/detalle_tour.html'  # Crea esta plantilla
+    context_object_name = 'tour'
+
 
 def agencias(request):
-    return render(request, 'agencias.html')
+    return render(request, 'tour_templates/agencias.html')
+
 
 def filtro_tour(request):
     place_of_origin = request.GET.get('place_of_origin', '')
