0268185a0e409037f2abf6a39dcdaf8ac1a6c6eb
Hectum <al202392@alumnos.uacj.mx>
2024-10-26 03:03:42 -0600

    Logica y estructura para la seccion de agencias
    Cambie un poco el diseño de la vista de las cards de los tours, ademas ya hice el esqueleto para todo lo relacionado a los tours, a lo mucho faltan la parte de las reviews pero eso lo hago en la tarde y ademas cada que entras a una agencia salen sus respectivos tours.

--- echeraritours/apps/appTour/views.py
+++ echeraritours/apps/appTour/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
 
-from .models import Tour
+from .models import Tour, Agency
 from django.db.models import Q
 
 # Create your views here.
@@ -29,12 +29,25 @@ def tours(request):
 
 class TourDetailView(DetailView):
     model = Tour
-    template_name = 'tour_templates/detalle_tour.html'  # Crea esta plantilla
+    template_name = 'tour_templates/detalle_tour.html'
     context_object_name = 'tour'
 
 
 def agencias(request):
-    return render(request, 'tour_templates/agencias.html')
+    agencias = Agency.objects.all()
+    return render(request, 'tour_templates/agencias.html', {'agencias': agencias})
+
+class AgencyDetailView(DetailView):
+    model = Agency
+
+    template_name = 'tour_templates/detalle_agencia.html' 
+    context_object_name = 'agencia'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Aqui hace que solo los tours relacionados con la agencia se vayan a mostrar
+        context['tours'] = Tour.objects.filter(agency=self.object)
+        return context
 
 
 def filtro_tour(request):
