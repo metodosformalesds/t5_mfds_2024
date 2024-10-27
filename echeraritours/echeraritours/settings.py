e3a4c55d1a4ad29f46acbf80049119b65a95d9a0
LeonardoOrtegaEscobar <143544582+LeonardoOrtegaEscobar@users.noreply.github.com>
2024-10-26 23:05:49 -0600

    Migracion de la base de datos a Mysql

--- echeraritours/echeraritours/settings.py
+++ echeraritours/echeraritours/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'echeraritours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Database-1',  # Nombre de la base de datos en MySQL
+        'USER': 'Leonardo',  # Nombre de usuario de la base de datos
+        'PASSWORD': 'echeraritours',  # La contraseña que creaste
+        'HOST': 'ls-d79a6da0aff3438baefae126ba22bb1cb9329666.cvya6wuiewji.us-west-2.rds.amazonaws.com',  # El endpoint de la base de datos
+        'PORT': '3306',  # Puerto por defecto de MySQL
     }
 }
 
